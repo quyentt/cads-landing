7add81bd824da6b03804df8d186d87cae241a7b7
quyentt <quyentt1985@gmail.com>
2024-01-24 16:33:50 +0700

    first commit

--- assets/js/customs.js
+++ assets/js/customs.js
@@ -34,4 +34,28 @@ $(document).ready(function () {
       clickable: true,
     },
   });
+  // $("#register").modal("show");
+
+  // Gets the video src from the data-src on each button
+
+  var $videoSrc;
+  $(".video-btn").click(function () {
+    $videoSrc = $(this).data("src");
+  });
+  console.log($videoSrc);
+
+  // when the modal is opened autoplay it
+  $("#video_pop").on("shown.bs.modal", function (e) {
+    // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+    $("#video").attr(
+      "src",
+      $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+    );
+  });
+
+  // stop playing the youtube video when I close the modal
+  $("#video_pop").on("hide.bs.modal", function (e) {
+    // a poor man's stop video
+    $("#video").attr("src", $videoSrc);
+  });
 });
